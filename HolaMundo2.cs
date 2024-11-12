ed09d6ebc8b1c7e9aeb9f22076ddbb9e57535b04
Jorge Hernandez Toledo <118739540+JorgeHdezT@users.noreply.github.com>
2024-11-12 16:50:06 +0000

    Primer Script
    Este script dice hola mundo

---
+++ HolaMundo2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string variable = "Hola";
+        print("sdsagds");
+        print(123124);
+        print(variable);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
